26192fb8260c95f496ec9aa252eb7c20e42af67e
PASIYA-MDv <pasidusampath120@gmail.com>
2025-10-28 13:33:35 +0530

    Delete plugins/owner.js

--- plugins/owner.js
+++
@@ -1,9 +0,0 @@
-const { ownerNumbers, botName } = require('../config')
-
-module.exports = {
-  command: 'owner',
-  execute: async (sock, msg, args) => {
-    const text = `Bot Name: ${botName}\nOwner(s):\n${ownerNumbers.join('\n')}`
-    await sock.sendMessage(msg.key.remoteJid, { text })
-  }
-}
